7668de716bff356c2842330c70b48ee2dad5d889
Akshit Sharma <32413264+AkshitSharma1@users.noreply.github.com>
2024-07-21 15:25:55 +0530

    Time: 52 ms (95.6%), Space: 46.6 MB (37.84%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,31 +1,23 @@
 class Solution {
 public:
-
     int subarraySum(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int> prefixSum(n+1,0);
         unordered_map<int,int> mp;
-        int i=0;
-        int j=0;
-        vector<int> prefixSum(nums.size()+1,-1);
-        mp[0]=1;
         int ans=0;
-        for(int j=0;j<nums.size();j++) {
-            if(j==0) {
-                prefixSum[j] = nums[0];
-            } else {
-            prefixSum[j] = prefixSum[j-1]+nums[j];
-            }
+        //Initialize base case - when sum of elements is exactly =k, 
+        //We can also pick subarray such that sum[0,j] =k and no smaller  subarray within [0,j] has sum=k
+        mp[0]=1;
+        for(int j=0;j<n;j++) {
+            //Calculate prefix sum 
+            if(j==0) { prefixSum[0] = nums[0];} else { prefixSum[j] = prefixSum[j-1]+nums[j];}
             int requiredSum = prefixSum[j]-k;
+            if(mp.find(requiredSum)!=mp.end()) ans+=mp[requiredSum]; 
+            //Say prefix sum at j= 50, k=30 we can form number of subarrays using prefix sum =20 encountered so far
+            mp[prefixSum[j]]+=1;
 
-            if(mp.find(requiredSum)!=mp.end()) { ans+=mp[requiredSum];
-            }
-                        mp[prefixSum[j]]+=1;
 
         }
-        
-
-        
-
-return ans;
-        
+        return ans;
     }
 };
