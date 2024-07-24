cc2371068f8d2c7e854f666aa32cb60988d5dab8
Akshit Sharma <32413264+AkshitSharma1@users.noreply.github.com>
2024-07-24 14:17:41 +0530

    Time: 50 ms (15.77%), Space: 38.6 MB (13.87%) - LeetHub

--- 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.cpp
+++ 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
     int numSubarraysWithSum(vector<int>& nums, int goal) {
-        //Here neither window size is given (ie. number of subarrays of size k) neither its given to find maximum/min window -> no sliding window
-        //But, here we have to find sum of continuous subarrays -> prefix sum
-        int n = nums.size();
-        vector<int> prefixSum(n,0);
-        unordered_map<int,int> mp;
-        int ans=0;
+        unordered_map<int,int> mp; //prefixsum,count
         mp[0]=1;
-        for(int j=0;j<n;j++) {
-            if(j==0) { prefixSum[j] = nums[0]; } else { prefixSum[j] = prefixSum[j-1]+nums[j];}
-            int reqSum = prefixSum[j]-goal;
-            if(mp.find(reqSum)!=mp.end()) ans+=mp[reqSum];
-            mp[prefixSum[j]]++;
+        int prefixSum=0;
+        int ans=0;
+        for(auto num:nums) {
+            prefixSum+=num;
+            int diff = prefixSum-goal;
+            if(mp.find(diff)!=mp.end()) ans+=mp[diff];
+            mp[prefixSum]++;
         }
-        return ans;
-        
+         return ans;
+
     }
 };
