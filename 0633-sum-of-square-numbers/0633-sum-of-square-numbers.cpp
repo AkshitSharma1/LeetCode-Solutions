f603d45c2902ce816410d3361f1d026c8b12788a
Akshit Sharma <32413264+AkshitSharma1@users.noreply.github.com>
2024-07-27 16:50:25 +0530

    Time: 55 ms (8.81%), Space: 82 MB (5.04%) - LeetHub

---
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
+        long long  size=pow(2,16)+1;
+        vector<long long > precomputePower(size,0);
+        for(long long  i=0;i<size;i++) {
+            precomputePower[i]=i*i;
+        }
+        long long  l =0;
+        long long  r = size-1;
+        long long sum=0;
+        while(l<=r) {
+            sum = precomputePower[l]+precomputePower[r];
+            if (sum==c) return true;
+            if(sum<c) {
+                l+=1;
+            } else {
+                r-=1;
+            }
+        }
+        return false;
+    }
+};
