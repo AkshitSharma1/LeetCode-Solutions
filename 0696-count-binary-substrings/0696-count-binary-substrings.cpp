383f1cf7ad30fb61bf39a8706c4ecba5d074b3c4
Akshit Sharma <32413264+AkshitSharma1@users.noreply.github.com>
2024-07-31 16:39:45 +0530

    Time: 16 ms (93.68%), Space: 12 MB (39.34%) - LeetHub

--- 0696-count-binary-substrings/0696-count-binary-substrings.cpp
+++ 0696-count-binary-substrings/0696-count-binary-substrings.cpp
@@ -1,53 +1,20 @@
 class Solution {
 public:
     int countBinarySubstrings(string s) {
-        int l=0;
-        int r=0;
-        int n = s.length();
-        unordered_map<int,int> mp;
-        int oneCount=0;
-        int zeroCount=0;
-        int diff=0;
+        int prev=0;
+        int curr=1;
         int ans=0;
-        mp[0]=1;
-        while(r<n) {
-            if(r>0 && s[r]=='0' && s[r]!=s[r-1]&&zeroCount>0) {
-                while(l<r && s[l]=='0') {
-                   zeroCount-=1;
-                    l+=1;
-                }
-                mp.clear();
-                mp[0]=1;
-                for(int i=1;i<=oneCount;i++) {
-                    mp[i]=1;
-                }
-                continue;
-            }
+        int n = s.size();
+        int count=0;
 
-
-
- if(r>0 && s[r]=='1' && s[r]!=s[r-1]&&oneCount>0) {
-                while(l<r && s[l]=='1') {
-                   oneCount-=1;
-                    l+=1;
-                }
-                mp.clear();
-                mp[0]=1;
-                for(int i=1;i<=zeroCount;i++) {
-                    mp[(-1)*i]=1;
-                }
-                continue;
-            }
-
-
-
-            if(s[r]=='0') {zeroCount++;} else { oneCount++;}
-            diff = oneCount-zeroCount;
-            if(mp.find(diff)!=mp.end()) ans+=mp[diff];
-            mp[diff]++;
-            r++;
+        for(int i=1;i<n;i++) {
+            if(s[i]==s[i-1]) { curr++; } else {      
+                    ans+=min(prev,curr);
+                    prev=curr; 
+                    curr=1; 
+                    }
         }
-        return ans;
+                    ans+=min(prev,curr);
+return ans;
     }
-
 };
