d929787e9de841a48eb3742ed8ccc27cbcd9e5c6
Akshit Sharma <32413264+AkshitSharma1@users.noreply.github.com>
2024-08-11 00:57:59 +0530

    Time: 33 ms (42.94%), Space: 29.9 MB (36.94%) - LeetHub

---
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+int f(vector<vector<int>>& dp,int l,int r,string& s) {
+    if(r<=l) return 0;
+    if(dp[l][r]!=-1) return dp[l][r];
+
+    if(s[l]==s[r]) {
+        return dp[l][r]=0+f(dp,l+1,r-1,s);
+    } else {
+        return dp[l][r]=1+min(f(dp,l+1,r,s),f(dp,l,r-1,s));
+    }
+}
+    int minInsertions(string s) {
+        int l=0;
+        int n = s.size();
+        int r = n-1;
+        vector<vector<int>> dp(r+1,vector<int>(r+1,-1));
+        return f(dp,l,r,s);
+        
+    }
+};
