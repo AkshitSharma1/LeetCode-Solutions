ed5cadfe20aab41ec54a68c309b56e0b0767949c
Akshit Sharma <32413264+AkshitSharma1@users.noreply.github.com>
2024-08-13 23:55:13 +0530

    Time: 66 ms (79.5%), Space: 43.7 MB (47.79%) - LeetHub

---
+++ 1436-get-watched-videos-by-your-friends/1436-get-watched-videos-by-your-friends.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    static bool comparator(pair<string,int>& a,pair<string,int>& b) {
+        if( a.second==b.second) {
+            return a.first<=b.first;
+        }
+        return a.second<=b.second;
+    }
+    vector<string> watchedVideosByFriends(vector<vector<string>>& watchedVideos, vector<vector<int>>& friends, int id, int level) {
+        int n = friends.size();
+        vector<int> visited(n,0);
+        queue<pair<int,int>> q;
+    
+        q.push({id,0});
+        vector<int> levelKFriends;
+        visited[id]=1;
+        while(!q.empty()) {
+            int personId = q.front().first;
+            int personLevel = q.front().second;
+            q.pop();
+            for(auto person:friends[personId]) {
+                if(!visited[person]) {
+                    if(personLevel+1==level) {
+                        levelKFriends.push_back(person);
+                        visited[person]=1;
+                    } else {
+                        q.push({person,personLevel+1});
+                        visited[person]=1;
+                    }
+                }
+            }
+        }
+
+        unordered_map<string,int> freq;
+        for(auto person:levelKFriends) {
+            for(auto watchedVideo:watchedVideos[person]) {
+                freq[watchedVideo]++; 
+       }
+        }
+
+   
+
+        vector<pair<string,int>> watchedVideoVector;
+        for(auto video:freq) {
+            watchedVideoVector.push_back({video.first,video.second});
+        }
+        sort(watchedVideoVector.begin(),watchedVideoVector.end(),comparator);
+        vector<string> ans;
+        for(auto watchedVideo:watchedVideoVector) ans.push_back(watchedVideo.first);
+        return ans;
+    }
+};
