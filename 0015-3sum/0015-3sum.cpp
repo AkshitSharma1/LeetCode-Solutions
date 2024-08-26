dacd4983a35de490a4d0f00b1d71559db005a8b6
Akshit Sharma <32413264+AkshitSharma1@users.noreply.github.com>
2024-08-27 02:12:08 +0530

    Time: 55 ms (86.35%), Space: 27.5 MB (71.72%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -2,37 +2,31 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         sort(nums.begin(),nums.end());
-  
-        int n = nums.size();
-        int target=0;
-        int i=0;
         int sum=0;
-        int j;
+        int n = nums.size();
         vector<vector<int>> ans;
-        for(int start=0;start<n;start++) {
-            if (start>0 && nums[start]==nums[start-1]) continue;
-            target=(-1)*nums[start];
-            //Two pointer
-            i = start+1;
-            j = n-1;
-            while(i<j) {
-                if(i>=n || j<0) break;
 
-                sum = nums[i]+nums[j];
-                if(sum==target) {
+        for(int start=0;start<n;start++) {
+            if(start>0 && nums[start]==nums[start-1]) { continue;}
+            sum=nums[start];
+            int i = start+1;
+            int j = n-1;
+            while(i<n && j>0 && i<j ) {
+               // if(i>start+1 && nums[i]==nums[i-1]) { i++; continue;}
+                if(sum+nums[i]+nums[j]==0) {
                     ans.push_back({nums[start],nums[i],nums[j]});
-                    i+=1;
-                    while (i<n && nums[i]==nums[i-1]) i+=1;
-                }  else if(sum>target) {
+                    i++;
+                    while(i<n && nums[i]==nums[i-1]) i++;
+                } else if(sum+nums[i]+nums[j]>0) {
+                    //We want to decrease sum
+                    //Move j left
                     j-=1;
                 } else {
                     i+=1;
                 }
             }
-            
-            
+
         }
         return ans;
-
     }
 };
