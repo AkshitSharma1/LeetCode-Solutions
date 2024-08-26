404b4f53751f612e11d1d6bc33b8f6def3acd013
Akshit Sharma <32413264+AkshitSharma1@users.noreply.github.com>
2024-08-27 00:22:31 +0530

    Time: 52 ms (27.93%), Space: 38 MB (19.61%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -1,62 +1,23 @@
 class Solution {
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        int indexOfX=0;
-        int minDiff=INT_MAX;
-        int minIndex=0;
-        int minElement=INT_MAX;
-        int i=0;
-        vector<int> ans;
-        for(auto num:arr) {
-            if(abs(num-x)<minDiff || (abs(num-x)==minDiff && num<minElement)) {
-                minIndex=i;
-                minElement=num;
-                minDiff = abs(num-x);
-            } 
-            i+=1;
-        }
-        cout<<"closest element: "<<minElement<<" with index "<<minIndex<<endl;
+        priority_queue<pair<int,int>> pq;
         int count=0;
-        int n=arr.size();
-        ans.push_back(arr[minIndex]);
-        count+=1;
-        if(count==k) return ans;
-        int lPointer=minIndex-1;
-        int rPointer=minIndex+1;
-        while(lPointer>=0 && rPointer<n) {
-            if(abs(arr[lPointer]-x)<abs(arr[rPointer]-x) ||
-             (abs(arr[lPointer]-x)==abs(arr[rPointer]-x) && arr[lPointer]<arr[rPointer])) {
-                ans.push_back(arr[lPointer]);
-                count+=1;
-                lPointer-=1;
-
-            } else {
-                ans.push_back(arr[rPointer]);
-                rPointer+=1;
-                count+=1;
-            }
-            if(count==k) break;
+        for(int i=0;i<arr.size();i++) {
+           if(count<k) {
+            pq.push({abs(arr[i]-x),arr[i]});
+            count++;
+           } else  if(count==k && !pq.empty()&&pq.top().first>abs(arr[i]-x)) {
+            pq.pop();
+            pq.push({abs(arr[i]-x),arr[i]});
+           }
         }
-        if(count==k) {
-            sort(ans.begin(),ans.end());
-            return ans;
+        vector<int> ans;
+        while(!pq.empty()) {
+            ans.push_back(pq.top().second);
+            pq.pop();
         }
-        if(count<k && lPointer<0) {
-            while(rPointer<n && count<k) {
-                ans.push_back(arr[rPointer]);
-                rPointer+=1;
-                count+=1;
-            }
-        } else if(count<k && rPointer==n) {
-            while(lPointer>=0 && count<k) {
-                ans.push_back(arr[lPointer]);
-                lPointer-=1;
-                count+=1;
-            }
-        }
-                    sort(ans.begin(),ans.end());
-
+        sort(ans.begin(),ans.end());
         return ans;
-        
     }
 };
