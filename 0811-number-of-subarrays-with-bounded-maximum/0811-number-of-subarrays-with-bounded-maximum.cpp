e85c14ec218f89250c5c37715ab39d521e6f7c8a
Akshit Sharma <32413264+AkshitSharma1@users.noreply.github.com>
2024-08-27 21:20:31 +0530

    Time: 132 ms (5%), Space: 64.7 MB (5%) - LeetHub

--- 0811-number-of-subarrays-with-bounded-maximum/0811-number-of-subarrays-with-bounded-maximum.cpp
+++ 0811-number-of-subarrays-with-bounded-maximum/0811-number-of-subarrays-with-bounded-maximum.cpp
@@ -1,24 +1,40 @@
 class Solution {
 public:
     int numSubarrayBoundedMax(vector<int>& nums, int left, int right) {
+        stack<pair<int,int>> NGLStack;
+        stack<pair<int,int>> NGRStack;
         int n = nums.size();
-        int l=-1;
-        long long ans=0;
-        int r=0;
-        int currentInRange=-1;
-        while(r<n) {
-            if(nums[r]>right) {
-                r++;
-                l=r-1;
-                currentInRange=l;
-            } else if(nums[r]>=left && nums[r]<=right) {
-                currentInRange=r;
-                ans+=r-l;
-                r++;
-            } else {
-                ans+=currentInRange-l;
-                r++;
+        vector<int> NGL(n,-1);
+        vector<int> NGR(n,n);
+        for(int i=0;i<n;i++) {
+            if(!NGLStack.empty()) {
+                while(!NGLStack.empty()&&NGLStack.top().first<nums[i]) {
+                    NGLStack.pop();
+                }
+                if(!NGLStack.empty()) {
+                    NGL[i] = NGLStack.top().second;
+                }
             }
+            NGLStack.push({nums[i],i});
+        }
+
+
+          for(int i=n-1;i>=0;i--) {
+            if(!NGRStack.empty()) {
+                while(!NGRStack.empty()&&NGRStack.top().first<=nums[i]) {
+                    NGRStack.pop();
+                }
+                if(!NGRStack.empty()) {
+                    NGR[i] = NGRStack.top().second;
+                }
+            }
+            NGRStack.push({nums[i],i});
+        }
+        int ans=0;
+        for(int i=0;i<n;i++) {
+            if(!(nums[i]<=right && nums[i]>=left)) continue;
+            cout<<i<<endl;
+            ans+=(NGR[i]-i)*(i-NGL[i]);
         }
         return ans;
     }
