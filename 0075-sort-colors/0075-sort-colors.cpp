cef1447423a0aa8fc5a4a17f71cb08f7c2587a1a
Akshit Sharma <32413264+AkshitSharma1@users.noreply.github.com>
2024-08-27 13:52:22 +0530

    Time: 5 ms (18.7%), Space: 10.4 MB (12.43%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
-    void sortArray(vector<int>&nums,int p1,int val) {
-        int n = nums.size();
-        int p2=1;
-        while(p1<n) {
-            p2 = p1+1;
-            if(nums[p1]!=val) { p1++; continue;}
-            if(nums[p1]==val) {
-                while(p2<n && nums[p2]==val) p2+=1;
-                if(p2==n) break;
-                while(p1<n&&p2<n&&nums[p1]==val&&nums[p2]!=val) {
-                    swap(nums[p1],nums[p2]);
-                    p1+=1;
-                    p2+=1;
-                }
-            }
+    void sortiT(vector<int>& nums,int oldColor,int newColor) {
+      int i=0;
+      while((i<nums.size() && nums[i]==newColor) || oldColor!=-1 && i<nums.size()&&nums[i]!=oldColor) i++;
+      int j=i+1;
+      while(j<nums.size()) {
+         if(oldColor!=-1 && i<nums.size()&&nums[i]!=oldColor)  {i++; j=i+1; continue; }
+
+        if(nums[j]==newColor) { 
+            swap(nums[j],nums[i]);
+            j++;
+            i++;
+        } else {
+            j++;
         }
+        
+      }
     }
     void sortColors(vector<int>& nums) {
-        sortArray(nums,0,0);
-        sortArray(nums,0,1);
-        sortArray(nums,0,2);
-
-        
+        sortiT(nums,-1,0);
+        sortiT(nums,2,1);
+      //  return nums;
     }
 };
