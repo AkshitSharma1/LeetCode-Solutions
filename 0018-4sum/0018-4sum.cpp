c95cac6c8130b43daee8349ed75073668a366bad
Akshit Sharma <32413264+AkshitSharma1@users.noreply.github.com>
2024-08-27 13:38:24 +0530

    Time: 28 ms (53.72%), Space: 16.2 MB (65.84%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,37 +1,29 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        sort(nums.begin(),nums.end());
-        long long l=0;
-        long long r=0;
-        long long n = nums.size();
-        long long sum=0;
+        long long goal = 0;
         vector<vector<int>> ans;
-        for(long long start1=0;start1<n;start1++) {
-            if(start1>=1 && nums[start1-1]==nums[start1]) continue;
-
-            for(long long start2=start1+1;start2<n;start2++) {
-                if(start2-start1>=2 && nums[start2-1]==nums[start2]) continue;
-                l=start2+1;
-                r = n-1;
-                while(l<r) {
-                    if (l>=n) break;
-                    sum= nums[start1];
-                    sum+=nums[start2];
-                    sum+=nums[l];
-                    sum+=nums[r];
-                    if(sum==target) {
-                        ans.push_back({nums[start1],nums[start2],nums[l],nums[r]});
-                        l+=1;
-                        while(l<n&& nums[l]==nums[l-1]) l+=1;
-                    } else if(sum>target) {
-                        r-=1;
-                    } else {
-                        l+=1;
-                    }
+        int n = nums.size();
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<n;i++) {
+            if (i>0 && nums[i]==nums[i-1]) continue;
+            goal =nums[i];
+            for(int j=i+1;j<n;j++) {
+                if(j>i+1 && nums[j]==nums[j-1]) continue;
+                goal=nums[i]+nums[j];
+                int start=j+1;
+                int end=n-1;
+                while(start<end) {
+                    if(start>j+1 && nums[start]==nums[start-1]) { start++; continue;}
+                    if(goal+nums[start]+nums[end]==target) {
+                        ans.push_back({nums[i],nums[j],nums[start],nums[end]});
+                        start++;
+                        while(start<n && nums[start]==nums[start-1]) { start++;}
+                    } else if(goal+nums[start]+nums[end]>target) {end--;} else { start++;}
                 }
+
             }
         }
-        return ans;
+    return ans;
     }
 };
