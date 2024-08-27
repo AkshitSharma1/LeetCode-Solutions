663f847c4aea57d0474ae5521fb020c42b9c8637
Akshit Sharma <32413264+AkshitSharma1@users.noreply.github.com>
2024-08-27 20:47:38 +0530

    Time: 95 ms (52.46%), Space: 71.4 MB (14.08%) - LeetHub

--- 0456-132-pattern/0456-132-pattern.cpp
+++ 0456-132-pattern/0456-132-pattern.cpp
@@ -1,30 +1,32 @@
 class Solution {
 public:
     bool find132pattern(vector<int>& nums) {
-        int n=nums.size();
-        vector<int> minElements(n,INT_MAX);
-        minElements[0]=nums[0];
-        for(int i=1;i<n;i++) {
-            minElements[i] = min(minElements[i-1],nums[i]);
-        }
+        int n = nums.size();
+        vector<int> minElementToLeft(n,0);
+        minElementToLeft[0]=nums[0];
+        for(int i=1;i<n;i++) minElementToLeft[i] = min(nums[i],minElementToLeft[i-1]);
+        //Stack to find NEXT GREATER ELEMENT TO LEFT
         stack<pair<int,int>> st;
-        vector<int> greaterOnLeftIndex(n,-1);
+        vector<int> NGLIndex(n,-1);
         for(int i=0;i<n;i++) {
-            if (st.empty()) {
+            if(st.empty()) {
                 st.push({nums[i],i});
             } else {
-                while(st.empty()==false && st.top().first<=nums[i]) {
+                while(!st.empty() && st.top().first<=nums[i]) {
                     st.pop();
                 }
-                if(st.empty()==false) {
-                    greaterOnLeftIndex[i]=st.top().second;
-                }
+                if(!st.empty()) {
+                    NGLIndex[i] = st.top().second;
+                    
+                } 
                 st.push({nums[i],i});
+
             }
         }
-      
         for(int k=n-1;k>0;k--) {
-            if(greaterOnLeftIndex[k]!=-1 && nums[greaterOnLeftIndex[k]]>nums[k]&&nums[greaterOnLeftIndex[k]]>minElements[greaterOnLeftIndex[k]]&&nums[k]>minElements[greaterOnLeftIndex[k]]) return true;
+            int j = NGLIndex[k];
+            if(j==0 || j==-1) continue;
+            if(minElementToLeft[j-1]<nums[k]) return true;
         }
         return false;
     }
