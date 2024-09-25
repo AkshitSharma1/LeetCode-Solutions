ef6f83321ed91ae5704536c5c4f2e036af1b47cb
Akshit Sharma <32413264+AkshitSharma1@users.noreply.github.com>
2024-09-25 18:55:21 +0530

    Time: 39 ms (48.82%), Space: 16.9 MB (12.96%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,14 +1,15 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         ans = []
-        def countPairs(currentString,unbalanceCount):
-            if len(currentString)==2*n:
-                if unbalanceCount==0: ans.append(currentString)
-                return
-            
-            countPairs(currentString+"(",unbalanceCount+1)
-            if unbalanceCount>0:
-                countPairs(currentString+")",unbalanceCount-1)
-        countPairs("",0)
-        return ans
         
+        def backtrack(currentString, openCount, closeCount):
+            if len(currentString) == 2 * n:
+                ans.append(currentString)
+                return
+            if openCount < n:
+                backtrack(currentString + "(", openCount + 1, closeCount)
+            if closeCount < openCount:
+                backtrack(currentString + ")", openCount, closeCount + 1)
+        
+        backtrack("", 0, 0)
+        return ans
