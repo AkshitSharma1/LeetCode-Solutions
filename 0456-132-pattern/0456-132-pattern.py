914bc06dbe9c194b03845008fce4041f6b3c1300
Akshit Sharma <32413264+AkshitSharma1@users.noreply.github.com>
2024-10-02 00:35:53 +0530

    Time: 598 ms (9.68%), Space: 37 MB (21.74%) - LeetHub

--- 0456-132-pattern/0456-132-pattern.py
+++ 0456-132-pattern/0456-132-pattern.py
@@ -1,28 +1,20 @@
 class Solution:
     def find132pattern(self, nums):
         n = len(nums)
-        minElementToLeft = [0] * n
-        minElementToLeft[0] = nums[0]
-        
-        for i in range(1, n):
-            minElementToLeft[i] = min(nums[i], minElementToLeft[i - 1])
-        
-        # Stack to find NEXT GREATER ELEMENT TO LEFT
         st = []
-        NGLIndex = [-1] * n
+        NGLIndex = [-1]*n
+        minElement = [nums[0]]*n
         
         for i in range(n):
-            while st and st[-1][0] <= nums[i]:
+            minElement[i] = min(minElement[i-1],nums[i])
+            while st and st[-1][0]<nums[i]:
                 st.pop()
             if st:
                 NGLIndex[i] = st[-1][1]
-            st.append((nums[i], i))
-        
-        for k in range(n - 1, 0, -1):
-            j = NGLIndex[k]
-            if j == 0 or j == -1:
-                continue
-            if minElementToLeft[j - 1] < nums[k]:
-                return True
-        
+            st.append((nums[i],i))
+        for i in range(n-1,-1,-1):
+            if NGLIndex[i]!=-1 and NGLIndex[i]!=0:
+                if minElement[NGLIndex[i]-1]<nums[i] and nums[NGLIndex[i]]>nums[i]:
+                    return True
         return False
+
