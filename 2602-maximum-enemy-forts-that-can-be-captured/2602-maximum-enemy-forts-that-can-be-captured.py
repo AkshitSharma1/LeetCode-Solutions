a457b9bbad7faca76fbb9081aa08b476d8276841
Akshit Sharma <32413264+AkshitSharma1@users.noreply.github.com>
2025-05-10 02:33:42 +0530

    Time: 0 ms (100%), Space: 18 MB (20.92%) - LeetHub

--- 2602-maximum-enemy-forts-that-can-be-captured/2602-maximum-enemy-forts-that-can-be-captured.py
+++ 2602-maximum-enemy-forts-that-can-be-captured/2602-maximum-enemy-forts-that-can-be-captured.py
@@ -1,39 +1,10 @@
 class Solution:
     def captureForts(self, forts: List[int]) -> int:
-        n = len(forts)
-        moveFromLeft = [-1]*n
-        moveFromRight = [-1]*n
-        
-        for i in range(n):
-            if forts[i]==1:
-                moveFromLeft[i]=0
-            if forts[i]==0:
-                if i>0:
-                    if moveFromLeft[i-1]!=-1:
-                        moveFromLeft[i]=moveFromLeft[i-1]+1
-                    else:
-                        moveFromLeft[i] = -1
-                else: moveFromLeft[i]=-1
-        for i in range(n-1,-1,-1):
-            if forts[i]==1:
-                moveFromRight[i]=0
-            if forts[i]==0:
-                if i<n-1:
-                    if moveFromRight[i+1]!=-1:
-                        moveFromRight[i] = moveFromRight[i+1]+1
-                    else:
-                        moveFromRight[i] = -1
-                else:
-                    moveFromRight[i] = -1
-        
-        ans=0
-
-        print(moveFromRight)
-        print(moveFromLeft)
-        for i in range(n):
-            if forts[i]==-1:
-                if i>0:
-                    ans = max(ans,moveFromLeft[i-1])
-                if i<n-1:
-                    ans = max(ans,moveFromRight[i+1])
-        return ans
+        count = 0
+        left = 0
+        for right in range(1,len(forts)):
+            if forts[right] != 0:
+                if (forts[left] == 1 and forts[right] == -1) or (forts[left] == -1 and forts[right] ==1):
+                    count = max(count, right - left - 1)
+                left = right
+        return count
