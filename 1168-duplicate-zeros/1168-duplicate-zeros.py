28b2dbd6d9f3abd11861e9a81ae2c11213fbc4a7
Akshit Sharma <32413264+AkshitSharma1@users.noreply.github.com>
2025-05-12 00:53:40 +0530

    Time: 1 ms (69.84%), Space: 17.9 MB (94.23%) - LeetHub

--- 1168-duplicate-zeros/1168-duplicate-zeros.py
+++ 1168-duplicate-zeros/1168-duplicate-zeros.py
@@ -1,12 +1,15 @@
 class Solution:
     def duplicateZeros(self, arr: List[int]) -> None:
+        # rightPointer will point at location where the value should come after relative shift
         rightPointer = len(arr)+arr.count(0)-1
+        #leftPointer points to value that should be copied
         leftPointer = len(arr)-1
         while leftPointer>=0:
             if rightPointer<len(arr):
                 arr[rightPointer]=arr[leftPointer]
             rightPointer-=1
             if arr[leftPointer]==0:
+                # Duplication case
                 if rightPointer<len(arr):
                     arr[rightPointer]=arr[leftPointer]
                 rightPointer-=1
