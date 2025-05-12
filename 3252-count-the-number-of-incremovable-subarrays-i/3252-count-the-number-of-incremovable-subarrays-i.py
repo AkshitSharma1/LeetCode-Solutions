0465992177497cebe1d8c0cf19efca6a7f315c9d
Akshit Sharma <32413264+AkshitSharma1@users.noreply.github.com>
2025-05-12 18:51:45 +0530

    Time: 103 ms (74.03%), Space: 18 MB (17.21%) - LeetHub

--- 3252-count-the-number-of-incremovable-subarrays-i/3252-count-the-number-of-incremovable-subarrays-i.py
+++ 3252-count-the-number-of-incremovable-subarrays-i/3252-count-the-number-of-incremovable-subarrays-i.py
@@ -1,10 +1,11 @@
 class Solution:
     def isIncremental(self,nums,ii,jj):
+        n = len(nums)
         for i in range(1,ii):
             if nums[i]<=nums[i-1]: return False
-        if ii-1>=0 and jj+1<len(nums):
+        if ii-1>=0 and jj+1<n:
             if nums[ii-1]>=nums[jj+1]: return False
-        for i in range(jj+2,len(nums)):
+        for i in range(jj+2,n):
             if nums[i]<=nums[i-1]: return False
         return True
 
@@ -16,7 +17,6 @@ class Solution:
         for i in range(n):
             for j in range(i,n):
                 if self.isIncremental(nums,i,j): 
-                    print(i,j)
                     answer+=1
         return answer
                 
