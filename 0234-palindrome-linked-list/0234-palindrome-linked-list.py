42431f8593d9ccca361d0e14d65395b65f3b9bdc
Akshit Sharma <32413264+AkshitSharma1@users.noreply.github.com>
2025-05-12 19:04:45 +0530

    Time: 36 ms (48.55%), Space: 34.9 MB (62.31%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.py
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.py
@@ -5,15 +5,31 @@
 #         self.next = next
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        stack = []
-        pointer = head
-        while pointer is not None:
-            stack.append(pointer.val)
-            pointer = pointer.next
-        
-        while len(stack)>0:
-            if stack.pop()!=head.val: return False
-            head = head.next
-        return True
+        # Aliter: O(N) time complexity + O(1) space complexity
+        '''
+        Find middle of Linkedlist using slow and fast pointer
+        Reverse 2nd half of linkedlist and compare head 2nd half head
+        '''
 
+        slowPointer = head
+        fastPointer = head
+        while  fastPointer is not None and fastPointer.next is not None:
+            slowPointer = slowPointer.next
+            fastPointer = fastPointer.next.next
+        
+        current,previous = slowPointer,None
+        while current is not None:
+            nextNode = current.next
+            current.next = previous
+            previous = current
+            current = nextNode
+        
+        secondHalfHead = previous
+        firstHalfHead = head
+        while firstHalfHead is not None and secondHalfHead is not None:
+            if firstHalfHead.val != secondHalfHead.val: return False
+            firstHalfHead = firstHalfHead.next
+            secondHalfHead = secondHalfHead.next
+        return True
+            
         
