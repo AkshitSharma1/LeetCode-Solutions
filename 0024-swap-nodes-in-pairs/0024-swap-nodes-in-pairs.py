62f81fc97805949fca48db80600cf12cfe91a500
Akshit Sharma <32413264+AkshitSharma1@users.noreply.github.com>
2025-05-14 17:12:19 +0530

    Time: 0 ms (100%), Space: 17.8 MB (33.47%) - LeetHub

--- 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.py
+++ 0024-swap-nodes-in-pairs/0024-swap-nodes-in-pairs.py
@@ -5,25 +5,17 @@
 #         self.next = next
 class Solution:
     def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        prevNode = ListNode(0,head)
-        start = prevNode
-        currNode = head
-        nextNode = head.next if head is not None else None
-        while nextNode is not None:
-          
+        if head is None or head.next is None: return head
+        prev = None
+        curr = head
 
-            #move all front by one
-            currNodeBackup = currNode.next.next if currNode is not None and currNode.next is not None and currNode.next.next is not None else None
-            nextNodeBackup = nextNode.next.next if nextNode is not None and nextNode.next is not None and nextNode.next.next is not None else None
-            prevNodeBackup = prevNode.next if prevNode is not None and prevNode.next is not None else None
-            
-            # handle middle node case
-            currNode.next = nextNode.next
-            nextNode.next = currNode
-            prevNode.next = nextNode
+        for _ in range(2):
+            nxt = curr.next
+            curr.next = prev
+            prev = curr
+            curr = nxt
 
-            currNode = currNodeBackup
-            nextNode = nextNodeBackup
-            prevNode = prevNodeBackup
-        return start.next
+        head.next = self.swapPairs(curr)
+
+        return prev
         
