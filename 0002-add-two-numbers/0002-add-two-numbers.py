d3c06ae5a46b0d3060cf0f4b08c3c19cbc848490
Akshit Sharma <32413264+AkshitSharma1@users.noreply.github.com>
2025-05-28 02:54:35 +0530

    Time: 7 ms (32.46%), Space: 17.7 MB (84.45%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -5,42 +5,26 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        # Find length of both linkedList
         l1Pointer = l1
         l2Pointer = l2
-        l2Length = 0
-        l1Length = 0
-        while l1Pointer is not None:
-            l1Length+=1
-            l1Pointer = l1Pointer.next
-        
-        while l2Pointer is not None:
-            l2Length+=1
-            l2Pointer = l2Pointer.next
-        base = l1
-        if l1Length>=l2Length:
-            p1, p2 = l1,l2
-            base = l1
-        else:
-            p1,p2 = l2,l1
-            base = l2
-        
+        dummy = ListNode(0,None)
+        head = dummy
+        carry=0
+        while l1Pointer is not None or l2Pointer is not None:
 
-        carry = 0
-        while p1 is not None:
-            sumOfValues = (p1.val + (p2.val if p2 is not None else 0) + carry)
-            p1.val = sumOfValues % 10
-            carry = sumOfValues // 10
+            sumOfValues = (l1Pointer.val if l1Pointer is not None else 0)+(l2Pointer.val if l2Pointer is not None else 0)
+            remainder = sumOfValues
+            dummy.next = ListNode((remainder+carry)%10,None)
+            dummy = dummy.next
+            carry = (remainder+carry)//10
 
-            print(carry)
 
-            if p1.next is None and carry!=0:
-                p1.next =  ListNode(val=0,next=None)
-                
-            p1 = p1.next
-            if p2 is not None:
-                p2 = p2.next
+            l1Pointer = l1Pointer.next if l1Pointer is not None else None
+            l2Pointer = l2Pointer.next if l2Pointer is not None else None
+
+        if carry!=0:
+            dummy.next = ListNode(1,None)
         
-        
-        return base
+        return head.next
+
         
