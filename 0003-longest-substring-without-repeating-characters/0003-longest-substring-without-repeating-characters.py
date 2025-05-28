74fe56507d83c26b4cd778aa507a409d2ffa8aa8
Akshit Sharma <32413264+AkshitSharma1@users.noreply.github.com>
2025-05-29 00:49:57 +0530

    Time: 32 ms (19.24%), Space: 17.8 MB (52.95%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,22 +1,20 @@
 from collections import Counter
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-
-        #Grow window
-        i = 0
-        n = len(s)
-        j = 0
-        ans=0
+        lPointer = 0
+        rPointer = 0
         counter = Counter()
-        while j<n:
-            counter[s[j]]+=1
-            j+=1
-            
-            while counter[s[j-1]]>1:
-                counter[s[i]]-=1
-                i+=1
-            ans = max(ans,j-i)
-        return ans
+        length = 0
+        for rPointer in range(len(s)):
+            counter[s[rPointer]]+=1
+            while lPointer<rPointer and counter[s[rPointer]]>1:
+                counter[s[lPointer]]-=1
+                lPointer+=1
 
+            length = max(length,rPointer-lPointer+1)
+        
+        return length
+                
+            
 
         
