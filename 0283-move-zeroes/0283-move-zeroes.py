ab5bca695158c6d35e66c24d0a920caa61883a8f
Akshit Sharma <32413264+AkshitSharma1@users.noreply.github.com>
2025-05-29 21:11:24 +0530

    Time: 7 ms (45.27%), Space: 18.9 MB (31.18%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -1,22 +1,22 @@
 class Solution:
-    def moveZeroes(self, arr: List[int]) -> None:
+    def moveZeroes(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-        
-        leftPointer = 0
-        rightPointer = 0
-        n = len(arr)
-        while rightPointer<n:
-            if arr[rightPointer]==0: 
-                rightPointer+=1
-            else:
-                arr[leftPointer]=arr[rightPointer]
-                leftPointer+=1
-                rightPointer+=1
-            
-        while leftPointer<n:
-            arr[leftPointer]=0
-            leftPointer+=1
-        
 
+        left,right=0,0
+        n = len(nums)
+        while right<n:
+            while right<n and nums[right]==0:
+                right+=1
+            if right==n: break
+
+            nums[left]=nums[right]
+            left+=1
+            right+=1
+
+        while left<n: 
+            nums[left]=0
+            left+=1
+        return nums
+        
