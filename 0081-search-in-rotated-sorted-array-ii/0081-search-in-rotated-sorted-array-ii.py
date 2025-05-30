832ad2eec85e40db4426b3bfb8ff4777f55a744e
Akshit Sharma <32413264+AkshitSharma1@users.noreply.github.com>
2025-05-30 14:47:10 +0530

    Time: 0 ms (100%), Space: 18.2 MB (49.9%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.py
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.py
@@ -5,18 +5,15 @@ class Solution:
         low = 0
         high = n-1
         while low<=high:
-            while low<n-1 and low<high and nums[low]==nums[low+1]:
-                low+=1
             
-            while high>0 and low<high and nums[high]==nums[high-1]:
-                high-=1
-            
-            if low>high: break
 
 
             mid = (low+high)//2
 
             if nums[mid]==target: return True
+            elif nums[low]==nums[mid] and nums[mid]==nums[high]:
+                low+=1
+            
             elif nums[low]<=nums[mid]:
                 #This portion is sorted
                 if nums[low]<=target and nums[mid]>=target:
