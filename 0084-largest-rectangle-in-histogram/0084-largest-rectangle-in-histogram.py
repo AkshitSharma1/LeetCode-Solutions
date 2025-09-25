6b773db52ff5a648c519e4727edf317cae4e6aca
Akshit Sharma <32413264+AkshitSharma1@users.noreply.github.com>
2025-09-25 17:47:44 +0530

    Time: 223 ms (17.43%), Space: 30.3 MB (56.57%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.py
@@ -1,23 +1,27 @@
 class Solution:
     def largestRectangleArea(self, heights: List[int]) -> int:
-        NSLStack,NSRStack = [],[]
         n = len(heights)
-        NSL = [-1]*n
-        NSR = [n]*n
-
-        #First we calculate NSR
+        NSR,NSL = [n]*n,[-1]*n
+        NSRStack,NSLStack = [],[]
         for i in range(n-1,-1,-1):
-            while len(NSRStack)>0 and heights[NSRStack[-1]]>=heights[i]:
+            while NSRStack and heights[NSRStack[-1]]>=heights[i]:
                 NSRStack.pop()
-            if len(NSRStack)>0:
+            if NSRStack:
                 NSR[i] = NSRStack[-1]
             NSRStack.append(i)
+        
         for i in range(0,n,1):
-            while len(NSLStack)>0 and heights[NSLStack[-1]]>=heights[i]: NSLStack.pop()
-            if len(NSLStack)>0: NSL[i]  = NSLStack[-1]
+            while NSLStack and heights[NSLStack[-1]]>=heights[i]:
+                NSLStack.pop()
+            if NSLStack:
+                NSL[i] = NSLStack[-1]
             NSLStack.append(i)
-        maxAnswer = 0
-        for i,height in enumerate(heights):
-            maxAnswer = max(maxAnswer,height*(NSR[i]-NSL[i]-1))
-        return maxAnswer
+        
+        maxArea = 0
+        for i in range(n):
+            maxArea = max(maxArea,(NSR[i]-NSL[i]-1)*heights[i])
+        return maxArea
+        
+
+
         
