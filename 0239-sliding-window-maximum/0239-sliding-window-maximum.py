7ff26f35911a46440f2d1c7f5a91ac34b0b082b5
Akshit Sharma <32413264+AkshitSharma1@users.noreply.github.com>
2025-09-25 14:15:18 +0530

    Time: 177 ms (44.32%), Space: 31.1 MB (70.12%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.py
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.py
@@ -1,14 +1,25 @@
 class Solution:
+
+
     def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
-        monotonicQueue = deque()
-        ans = []
-        for r,num in enumerate(nums):
-            while monotonicQueue and nums[monotonicQueue[-1]]<num:
-                monotonicQueue.pop()
-            monotonicQueue.append(r)
-            while monotonicQueue and monotonicQueue[0]<r-k+1:
-                monotonicQueue.popleft()
-            if r-k+1>=0:
-                ans.append(nums[monotonicQueue[0]])
-        return ans
+        slidingWindow = deque()
+        n = len(nums)
+        l = 0
+        answer = []
+        for r in range(n):
+            while slidingWindow and nums[slidingWindow[-1]]<nums[r]:
+                slidingWindow.pop()
+            slidingWindow.append(r)
+
+            if r>=k-1:
+                while slidingWindow and slidingWindow[0]<r-k+1:
+                    slidingWindow.popleft()
+
+                if slidingWindow: 
+                    answer.append(nums[slidingWindow[0]])
+            
+        return answer
+
+
+
         
