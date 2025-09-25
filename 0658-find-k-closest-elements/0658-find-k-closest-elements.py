74e1471897392ed74a2d3902e63bd513ab9a4d1b
Akshit Sharma <32413264+AkshitSharma1@users.noreply.github.com>
2025-09-25 19:02:51 +0530

    Time: 95 ms (5.53%), Space: 19.5 MB (21.73%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.py
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.py
@@ -1,18 +1,21 @@
+import math
 class Solution:
+    def dist(self,a,b):
+        return math.fabs(a-b)
     def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
-        maxHeap = []
-        heapq.heapify(maxHeap)
+        self.maxHeap = []
+        heapq.heapify(self.maxHeap)
         for num in arr:
-            dist = abs(x-num)
-            if len(maxHeap)==k:
-                if dist<-maxHeap[0][0]:
-                    heapq.heappop(maxHeap)
-                    heapq.heappush(maxHeap,(-dist,num))
+            if len(self.maxHeap)<k:
+                heapq.heappush(self.maxHeap,(-self.dist(num,x),num))
             else:
-                heapq.heappush(maxHeap,(-dist,num))
-        ans=[]
-        while len(maxHeap)>0:
-            ans.append(heapq.heappop(maxHeap)[1])
-        return sorted(ans)
-
+                maxDistElement = -self.maxHeap[0][0]
+                if maxDistElement>self.dist(num,x) or (maxDistElement==self.dist(num,x) and num<self.maxHeap[0][1]): 
+                    heapq.heappop(self.maxHeap)
+                    heapq.heappush(self.maxHeap,(-self.dist(num,x),num))
         
+        answer = []
+        while self.maxHeap:
+            answer.append(heapq.heappop(self.maxHeap)[1])
+        return sorted(answer)
+
