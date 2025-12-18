36e0f37a306f153b5a30eb91db0e55f8fb09c5a9
Akshit Sharma <32413264+AkshitSharma1@users.noreply.github.com>
2025-12-18 19:32:47 +0530

    Time: 0 ms (100%), Space: 17.1 MB (99.98%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -1,7 +1,7 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
         n = len(s)
-        dp = [0]*(n+1)
+        dp = defaultdict(int)
         dp[n] = 1
    
         for j in range(n-1,-1,-1):
