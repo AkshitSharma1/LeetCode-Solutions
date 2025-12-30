35290f9f4f2a3c4e637cb0d7be1f12f3da071eb7
Akshit Sharma <32413264+AkshitSharma1@users.noreply.github.com>
2025-12-30 14:40:31 +0530

    Time: 390 ms (56.2%), Space: 17.5 MB (89.53%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -1,41 +1,29 @@
 class Solution:
     def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
-        start = 0
+        current_sum = 0
+        p,q,l,r = 0,0,0,0
+        answer = []
         nums.sort()
         n = len(nums)
-        ans = []
-        while start<n:
-            if start>0 and start<n  and nums[start]==nums[start-1]:
-                start+=1
-                continue
-            
-
-            pointerTwo = start + 1
-            while pointerTwo<n:
-                if pointerTwo>start+1 and pointerTwo<n and nums[pointerTwo]==nums[pointerTwo-1]:
-                    pointerTwo+=1
-                    continue
-                
-                left = pointerTwo+1
-                right = n-1
-                while left<right:
-                    currentSum = nums[start]+nums[pointerTwo]+nums[left]+nums[right]
-                    if currentSum>target:
-                        right-=1
-                    elif currentSum<target:
-                        left+=1
+        for p in range(n):
+            if p>0 and nums[p]==nums[p-1]: continue
+            for q in range(p+1,n):
+                if q>p+1 and nums[q]==nums[q-1]: continue
+                l = q+1
+                r = n-1
+                while l<r:
+                    current_sum = nums[p]+nums[q]+nums[l]+nums[r]
+                    if current_sum>target:
+                        r-=1
+                    elif current_sum<target:
+                        l+=1
                     else:
-                        ans.append([nums[start],nums[pointerTwo],nums[left],nums[right]])
-                        left+=1
-                        right-=1
-                        while nums[left]==nums[left-1] and left<right:
-                            left+=1
-                        while nums[right]==nums[right+1] and left<right:
-                            right-=1
-                pointerTwo+=1
-            start+=1
-        return ans
-
+                        answer.append([nums[p],nums[q],nums[l],nums[r]])
+                        l+=1
+                        r-=1
+                        while l>q+1 and l<n and nums[l]==nums[l-1]: 
+                            l+=1
+                        while r<n-1 and r>=0 and nums[r]==nums[r+1]:
+                            r-=1
+        return answer
                     
-
-        
