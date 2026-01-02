8bc788357fbbcab9b59a780141c8ca2247743953
Akshit Sharma <32413264+AkshitSharma1@users.noreply.github.com>
2026-01-03 00:10:13 +0530

    Time: 701 ms (5.54%), Space: 53.1 MB (5%) - LeetHub

---
+++ 0502-ipo/0502-ipo.py
@@ -0,0 +1,22 @@
+class Solution:
+    def findMaximizedCapital(self, k: int, w: int, profits: List[int], capital: List[int]) -> int:
+        queue = deque(sorted([(c,p) for c,p in zip(capital,profits)],key=lambda x:(x[0],-x[1])))
+        max_heap = [] #(profit,capital)
+        pick_count = 0
+        while queue or max_heap:
+            while queue and queue[0][0]<=w:
+                cost,profit = queue.popleft()
+                heapq.heappush(max_heap,(-profit,-cost))
+            
+            if  len(max_heap)==0: return w
+
+            profit, cost = heapq.heappop(max_heap)
+            profit *=-1
+            cost *=-1 
+            pick_count+=1
+            w += profit
+            if pick_count==k: return w
+        return w
+
+
+            
