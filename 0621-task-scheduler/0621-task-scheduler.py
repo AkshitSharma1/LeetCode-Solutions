b9e54b5cac4e44e36e13702fd2ff5a6df71dfff9
Akshit Sharma <32413264+AkshitSharma1@users.noreply.github.com>
2026-01-02 14:04:51 +0530

    Time: 211 ms (10.09%), Space: 21.4 MB (5.1%) - LeetHub

---
+++ 0621-task-scheduler/0621-task-scheduler.py
@@ -0,0 +1,39 @@
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        task_frequency = Counter(tasks)
+        task_names = task_frequency.keys()
+        answer = []
+        queue = deque() #will contain [next_execution_time,frequency,task]
+        max_heap =[]
+        heapq.heapify(max_heap)
+
+        for task in task_names:
+            queue.append((0,task_frequency[task],task))
+        
+        #max frequency heap via (task_frequency,task)
+        time = 0
+        while queue or max_heap:
+            while queue and queue[0][0]<=time:
+                execution_time,frequency,task =  queue.popleft()
+                heapq.heappush(max_heap,(-frequency,execution_time,task))
+            
+            if len(max_heap)==0:
+                #No time to run at this time
+                for i in range(queue[0][0]-time):
+                    answer.append("IDLE")
+                time = queue[0][0]
+
+                continue
+            
+            frequency,execution_time,task = heapq.heappop(max_heap)
+            frequency = -frequency
+            answer.append(task)
+            frequency-=1
+            if frequency>0:
+                queue.append((time+n+1,frequency,task))
+            time+=1
+        print(answer)
+        return time
+
+
+            
