276211c7e3bc7d6f863e4f64004acad687b3f6a5
Akshit Sharma <32413264+AkshitSharma1@users.noreply.github.com>
2026-01-03 00:11:32 +0530

    Time: 562 ms (9.72%), Space: 53.2 MB (5%) - LeetHub

--- 0502-ipo/0502-ipo.py
+++ 0502-ipo/0502-ipo.py
@@ -1,21 +1,20 @@
-import heapq
-from typing import List
-
 class Solution:
     def findMaximizedCapital(self, k: int, w: int, profits: List[int], capital: List[int]) -> int:
-        projects = sorted(zip(capital, profits))  # sort by required capital
-        max_heap = []
-        i = 0
-        n = len(projects)
+        queue = deque(sorted([(c,p) for c,p in zip(capital,profits)],key=lambda x:(x[0],-x[1])))
+        max_heap = [] #(profit,capital)
+        pick_count = 0
+        while queue or max_heap:
+            while queue and queue[0][0]<=w:
+                cost,profit = queue.popleft()
+                heapq.heappush(max_heap,(-profit,-cost))
+            
+            if  len(max_heap)==0: return w
 
-        for _ in range(k):
-            while i < n and projects[i][0] <= w:
-                heapq.heappush(max_heap, -projects[i][1])  # max-heap via negative
-                i += 1
-
-            if not max_heap:
-                break
-
-            w += -heapq.heappop(max_heap)
-
+            profit, cost = heapq.heappop(max_heap)
+            profit *=-1
+            cost *=-1 
+            pick_count+=1
+            w += profit
+            if pick_count==k: return w
         return w
+
