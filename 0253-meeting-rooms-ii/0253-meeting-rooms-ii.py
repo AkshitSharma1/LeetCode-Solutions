c3451df67409f28399cc7d1947dce4b751554c2a
Akshit Sharma <32413264+AkshitSharma1@users.noreply.github.com>
2026-01-04 19:39:38 +0530

    Time: 3 ms (86.97%), Space: 19.4 MB (92.3%) - LeetHub

--- 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
+++ 0253-meeting-rooms-ii/0253-meeting-rooms-ii.py
@@ -1,14 +1,20 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
 class Solution:
-    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        concurrentMeetCount = defaultdict(int)
-        for interval in intervals:
-            concurrentMeetCount[interval[0]]+=1
-            concurrentMeetCount[interval[1]]-=1
-        
-        #Now sort this
-        maxConcurrentMeet = 0
+    def minMeetingRooms(self, intervals) -> int:
+        intervals.sort(key=lambda x: x[0])
+        min_heap = []
         answer = 0
-        for time in sorted(concurrentMeetCount.keys()):
-            maxConcurrentMeet+=concurrentMeetCount[time]
-            answer = max(answer,maxConcurrentMeet)
+        for interval in intervals:
+            while  min_heap and min_heap[0] <= interval[0]:
+                heapq.heappop(min_heap)
+            heapq.heappush(min_heap, interval[1])
+            answer = max(len(min_heap),answer)
+
         return answer
