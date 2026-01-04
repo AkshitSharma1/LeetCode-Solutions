e73f997905019841e273e94f1b5cf9468d81c6a0
Akshit Sharma <32413264+AkshitSharma1@users.noreply.github.com>
2026-01-04 14:40:50 +0530

    Time: 341 ms (59.18%), Space: 58.8 MB (50.55%) - LeetHub

--- 1834-single-threaded-cpu/1834-single-threaded-cpu.py
+++ 1834-single-threaded-cpu/1834-single-threaded-cpu.py
@@ -1,19 +1,24 @@
 class Solution:
     def getOrder(self, tasks: List[List[int]]) -> List[int]:
-        #[[enquietime, processingtime]]
-        queue = deque(sorted([(e,p,i) for i,(e,p) in enumerate(tasks)],key=lambda x:(x[0],x[1],x[2])))
-        min_heap = []
-        curr_time = 0
+        #n tasks
+        #each task- [enquietime i, processtinjg time p]
+        queue = deque(sorted([(e,p,i) for i,(e,p) in enumerate(tasks)]
+            ,key=lambda x:(x[0],x[1],x[2]))
+        )
+        min_heap = [] #([processing time, index])
+        time = 0
         answer = []
-        while min_heap or queue:
-            while queue and queue[0][0]<=curr_time:
+        while queue or min_heap:
+            while queue and queue[0][0]<=time:
                 e,p,i = queue.popleft()
                 heapq.heappush(min_heap,(p,i))
-            if min_heap:
-                processing_time, index = heapq.heappop(min_heap)
-                curr_time += processing_time
-                answer.append(index)
-            else:
-                curr_time = queue[0][0]
+            
+            if not min_heap:
+                time = queue[0][0] 
                 continue
+            
+            p,i = heapq.heappop(min_heap)
+            answer.append(i)
+            time+=p
+        
         return answer
