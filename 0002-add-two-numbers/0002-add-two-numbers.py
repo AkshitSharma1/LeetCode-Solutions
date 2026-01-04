15144aa72562ab60b2ce02eef604aabc1022285b
Akshit Sharma <32413264+AkshitSharma1@users.noreply.github.com>
2026-01-05 01:10:21 +0530

    Time: 8 ms (19.29%), Space: 17.5 MB (76.75%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -3,28 +3,23 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
 class Solution:
-    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        l1Pointer = l1
-        l2Pointer = l2
-        dummy = ListNode(0,None)
-        head = dummy
-        carry=0
-        while l1Pointer is not None or l2Pointer is not None:
+    def addTwoNumbers(self, l1, l2):
+        dummy = ListNode(-1)
+        cur = dummy
+        carry = 0
 
-            sumOfValues = (l1Pointer.val if l1Pointer is not None else 0)+(l2Pointer.val if l2Pointer is not None else 0)
-            remainder = sumOfValues
-            dummy.next = ListNode((remainder+carry)%10,None)
-            dummy = dummy.next
-            carry = (remainder+carry)//10
+        while carry or (l1 or l2):
+            v1 = l1.val if l1 else 0
+            v2 = l2.val if l2 else 0
 
+            s = v1 + v2+ carry
+            carry = s // 10
+            cur.next = ListNode(s % 10)
+            cur = cur.next
 
-            l1Pointer = l1Pointer.next if l1Pointer is not None else None
-            l2Pointer = l2Pointer.next if l2Pointer is not None else None
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
 
-        if carry!=0:
-            dummy.next = ListNode(1,None)
-        
-        return head.next
-
-        
+        return dummy.next
