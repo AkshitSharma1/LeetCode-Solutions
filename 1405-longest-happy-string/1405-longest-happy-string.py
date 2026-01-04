a4303a7c799c72cb5c284f6034526a45f397f40e
Akshit Sharma <32413264+AkshitSharma1@users.noreply.github.com>
2026-01-04 13:47:00 +0530

    Time: 0 ms (100%), Space: 17.3 MB (90.73%) - LeetHub

--- 1405-longest-happy-string/1405-longest-happy-string.py
+++ 1405-longest-happy-string/1405-longest-happy-string.py
@@ -1,28 +1,28 @@
 class Solution:
-    def _get_max_freq_char(self,freq_count,avoid_char='X'):
-        for char,freq in freq_count.most_common():
-            if char==avoid_char:
-                continue
-            else:
-                if freq!=0:
-                    freq_count[char]-=1
-                    return char
-        return '-1'
-
     def longestDiverseString(self, a: int, b: int, c: int) -> str:
-        max_heap = []
-        # (frequency_count, char) of size 3
-        frequency_count = Counter({"a":a,"b":b,"c":c})
         s = ""
-        while True:
-            avoid_char = None
-            if s and len(s)>=2 and s[-2]==s[-1]:
-                avoid_char = s[-1]
-            get_next_char = self._get_max_freq_char(frequency_count,avoid_char)
-            if get_next_char=='-1':
-                return s
+        max_heap = []
+        heapq.heapify(max_heap)
+        for char,count in [("a",a),("b",b),("c",c)]:
+            if count==0: continue
+            heapq.heappush(max_heap,(-count,char))
+        while max_heap:
+            freq, char = heapq.heappop(max_heap)
+            if len(s)>=2 and s[-2]==s[-1]==char:
+                #pop another
+                if not max_heap: return s
+                freq2, char2 = heapq.heappop(max_heap)
+                s+=char2
+                freq2+=1
+                if freq2<0: 
+                    heapq.heappush(max_heap,(freq2,char2))
             else:
-                s+= get_next_char
-        return -1
-    
-             
+                s+=char
+                freq+=1
+            
+            if freq<0:
+                heapq.heappush(max_heap,(freq,char))
+        return s
+            
+            
+
