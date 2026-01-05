d2b9fb1a3d7d7e54e2a8729f604a201d92b9c86f
Akshit Sharma <32413264+AkshitSharma1@users.noreply.github.com>
2026-01-05 20:02:35 +0530

    Time: 7 ms (4.52%), Space: 17.8 MB (34.61%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,10 +1,48 @@
+class TrieNode():
+    def __init__(self):
+        self.pass_count = 0
+        self.end_count = 0
+        self.children = dict()
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+        self._size = 0
+
+    def insert(self,word):
+        node = self.root
+        node.pass_count+=1
+        self._size+=1
+        for c in word:
+            if c not in node.children:
+                node.children[c] = TrieNode()
+            node = node.children[c]
+            node.pass_count+=1
+        node.end_count+=1
+    
+    def _walk(self,word):
+        node = self.root
+        for c in word:
+            if c in node.children:
+                node = node.children[c]
+            else:
+                return None
+        return node
+    
+    def common_prefix(self):
+        #will return the longest prefix
+        prefix = []
+        node = self.root
+        while node and len(node.children)==1 and node.end_count==0:
+            (char,child), = node.children.items()
+            prefix.append(char)
+            node = child
+        return "".join(prefix)
+    
+
+
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        n = len(strs)
-        pointers = [0 for _ in range(n)]
-        strs.sort()
-        for i,curr_char in enumerate(strs[0]):
-            for other_string in strs[1:]:
-                if curr_char!=other_string[i]:
-                    return strs[0][:i]
-        return strs[0]
+        trie = Trie()
+        for word in strs: trie.insert(word)
+        return trie.common_prefix()
+       
