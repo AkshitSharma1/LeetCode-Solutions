2cc41e3630af48f51f14d9c15524c4ad86c5ee1a
Akshit Sharma <32413264+AkshitSharma1@users.noreply.github.com>
2026-01-17 20:26:59 +0530

    Time: 5 ms (77.46%), Space: 19.5 MB (5.43%) - LeetHub

---
+++ 1055-shortest-way-to-form-string/1055-shortest-way-to-form-string.py
@@ -0,0 +1,29 @@
+class Solution:
+    def shortestWay(self, source: str, target: str) -> int:
+        char_pos = defaultdict(list)
+        for i,ch in enumerate(source): char_pos[ch].append(i)
+        target_index = 0
+        src_index = -1
+        curr_substr_length = 0
+        answer = 0
+        while target_index<len(target):
+            target_ch = target[target_index]
+            if target_ch not in source: return -1
+            src_pos = char_pos[target_ch]
+            index = bisect_right(src_pos,src_index)
+            if index==len(src_pos):
+                #we cant make use of source further to make substring
+                src_index = -1
+                answer+=1
+                curr_substr_length = 0
+            else:
+                src_index = src_pos[index]
+                curr_substr_length +=1
+                target_index+=1
+        
+        if curr_substr_length>0: answer+=1
+        return answer
+
+
+            
+        
